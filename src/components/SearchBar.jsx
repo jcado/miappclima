87e6a9a108950dac916ca1874da1bb7b2a34701b
john rafael carrillo dolugar <ganarconseo@gmail.com>
2022-05-09 14:54:05 -0500

    se agrega el proyecto

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SearchBar(props) {
+  // acá va tu código
+  return (
+    <form>
+      <input type="text" placeholder='Ciudad...' />
+      <input type="submit" value="Agregar" />
+    </form>
+  )
+};
