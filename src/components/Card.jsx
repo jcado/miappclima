ad96e644a64e481e8b6e2c67d03476f4d9ad397e
john rafael carrillo dolugar <ganarconseo@gmail.com>
2022-05-11 07:57:44 -0500

    segunda modificacion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
-export default function Card(max, min, name, onClick) {
+
+export default function Card({name, min, max, img, onClose}) {
   // acá va tu código
-  return (<div>
+    return (
+    <>
       <h1>{name}</h1>
-    </div>)
+      <h3>MIN</h3>
+      <h3>{min}</h3>
+      <h3>MAX</h3>
+      <h3>{max}</h3>
+
+      <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="weather image" />
+      <button onClick={onClose}> X </button>
+    </>
+  )
 };
